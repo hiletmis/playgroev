d68cde95272ce9a06ea3884a297597b5c84085b9
hiletmis <hiletmis@gmail.com>
2024-05-27 16:24:37 +0300

    Add utility functions and color constants

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -4,3 +4,23 @@ export const parse = (value: any) => {
     if (value === undefined) return '0';
     return formatUnits(value, 18);
 }
+
+export const COLORS = {
+    app: '#f2f2f2',
+    appDarker: '#ffffff',
+    bg: '#ffffff',
+    table: "blue.900",
+    info: "gray.500",
+    main: "#ffffff",
+    button: "blue.700",
+    buttonDisabled: "gray.800",
+    caution: "yellow.700",
+};
+
+export const copy = (text: string) => {
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText(text).catch(err => { console.error('Failed to write clipboard contents: ', err) });
+    } else {
+        alert("Clipboard API not available");
+    }
+}
