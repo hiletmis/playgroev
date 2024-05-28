7395043835cc9e590a1dba825a8121be77d532be
hiletmis <hiletmis@gmail.com>
2024-05-28 11:17:43 +0300

    Add WrongNetwork component for displaying wrong network message

---
+++ src/custom/WrongNetwork.tsx
@@ -0,0 +1,16 @@
+import { VStack, Heading, Flex, Spacer, Text, Image } from '@chakra-ui/react';
+
+const Hero = () => {
+    return (
+        <VStack spacing={4} alignItems={"left"} >
+            <Flex>
+                <Heading size={"lg"}>Wrong Network</Heading>
+                <Spacer />
+                <Image src={`/caution.svg`} width={"30px"} height={"30px"} />
+            </Flex>
+            <Text fontSize={"sm"}>Please change network to OEV Chain.</Text>
+        </VStack>
+    );
+};
+
+export default Hero;
