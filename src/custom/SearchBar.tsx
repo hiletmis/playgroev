68200ca85fd56710b755b7919e984447c9e65799
hiletmis <hiletmis@gmail.com>
2024-05-29 11:56:37 +0300

    Add SearchBar component for search functionality

---
+++ src/custom/SearchBar.tsx
@@ -0,0 +1,26 @@
+
+import {
+    VStack,
+    Input,
+    InputGroup,
+    InputLeftElement,
+} from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
+
+export const SearchBar = (props: any) => {
+    const stateChanger = props.stateChanger;
+    return (
+        <VStack alignItems={"left"} width={"100%"} margin={2}>
+
+            <InputGroup size="md">
+                <InputLeftElement
+                    pointerEvents="none"
+                    children={<Search2Icon color="gray.600" />}
+                />
+                <Input type="text" placeholder="Search..." border="1px solid #949494"
+                    onChange={(e) => stateChanger(e.target.value)}
+                />
+            </InputGroup>
+        </VStack>
+    );
+};
