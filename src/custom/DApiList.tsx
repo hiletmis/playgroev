33efa7bd244032f0dfebd9c8ee8b9f044f3400a3
hiletmis <hiletmis@gmail.com>
2024-05-29 11:57:12 +0300

    Add DApiList component for displaying DAPI and chain information

---
+++ src/custom/DApiList.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import { Flex, VStack } from "@chakra-ui/react";
+import { getDapis, getChains } from '../helpers/utils';
+import DApiRow from "./DApiRow";
+import ChainRow from "./ChainRow";
+import { SearchBar } from "./SearchBar";
+
+const DataFeedList = (props: any) => {
+    const { dApi, setDapi, selectedChain, setSelectedChain } = props;
+    const [keyword, setKeyword] = useState("");
+
+    const [isDApiListVisible, setIsDApiListVisible] = useState(false);
+    const [isChainListVisible, setIsChainListVisible] = useState(false);
+
+    useEffect(() => {
+        setIsDApiListVisible(false);
+        setIsChainListVisible(false);
+    }, [dApi, selectedChain]);
+
+    useEffect(() => {
+        setKeyword("");
+        if (!isDApiListVisible) return
+        setIsChainListVisible(false);
+    }, [isDApiListVisible]);
+
+    useEffect(() => {
+        setKeyword("");
+        if (!isChainListVisible) return
+        setIsDApiListVisible(false);
+    }, [isChainListVisible]);
+
+    return (
+        <VStack width={"100%"} spacing={3} alignItems={"left"}>
+            <Flex width={"100%"} justifyContent={"space-between"} gap={2}>
+                <ChainRow selectedChain={selectedChain} setSelectedChain={setSelectedChain} isHeader={true} isOpen={isChainListVisible} onClick={() => { setIsChainListVisible(!isChainListVisible) }}></ChainRow>
+                <DApiRow dApi={dApi} setDapi={setDapi} isHeader={true} isOpen={isDApiListVisible} onClick={() => { setIsDApiListVisible(!isDApiListVisible) }}></DApiRow>
+            </Flex>
+
+            {
+                isDApiListVisible &&
+                <VStack width={"100%"} p={5} maxHeight={"350px"} overflow={"scroll"} spacing={3} boxShadow={"md"} borderRadius={"10px"} >
+                    <SearchBar stateChanger={setKeyword}></SearchBar>
+
+                    {
+                        getDapis().filter((dApi) => { return dApi.name.toLowerCase().includes(keyword.toLowerCase()) }).map((dApi, index) => {
+                            return (
+                                <DApiRow key={index} dApi={dApi} setDapi={setDapi}></DApiRow>
+                            )
+                        })
+                    }
+                </VStack>
+            }
+
+            {
+                isChainListVisible &&
+                <VStack width={"100%"} p={5} maxHeight={"350px"} overflow={"scroll"} spacing={3} boxShadow={"md"} borderRadius={"10px"} >
+                    <SearchBar stateChanger={setKeyword}></SearchBar>
+
+                    {
+                        getChains().filter((chain) => { return chain.name.toLowerCase().includes(keyword.toLowerCase()) }).map((chain, index) => {
+                            return (
+                                <ChainRow key={index} selectedChain={chain} setSelectedChain={setSelectedChain}></ChainRow>
+                            )
+                        })
+                    }
+                </VStack>
+            }
+
+        </VStack>
+    )
+};
+
+export default DataFeedList;
