9fb8db49e3a3e6ff9fb86f123693b07455340f73
hiletmis <hiletmis@gmail.com>
2024-05-29 11:56:53 +0300

    Add DApiRow component for displaying DAPI information

---
+++ src/custom/DApiRow.tsx
@@ -0,0 +1,40 @@
+import { Text, Stack, Box, Image, Flex, Spacer, VStack } from '@chakra-ui/react';
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
+import { COLORS } from '../helpers/utils';
+import { SymbolLogo } from '@api3/logos';
+
+const Hero = (props: any) => {
+    const dApiName = props.dApi.name;
+    const isHeader = props.isHeader || false;
+    const isOpen = props.isOpen || false;
+    const symbols = dApiName.split('/');
+
+    return (
+        <VStack width={"100%"} alignItems={"left"} cursor={"pointer"} onClick={isHeader ? props.onClick : () => { props.setDapi(props.dApi) }}>
+            <Box p="3" width={"100%"} height={"70px"} borderRadius={"10"} bgColor={COLORS.app} alignItems={"left"}>
+                <Flex className='box' alignItems={"center"}>
+                    <Stack direction="column" spacing={"2"} width={"100%"}>
+                        <Stack direction="row" spacing={"2"} >
+                            <Stack direction="row" spacing={"1"}>
+                                <Image src={SymbolLogo(symbols[0], true)} width={"24px"} height={"24px"} />
+                                <Image src={SymbolLogo(symbols[1], true)} width={"24px"} height={"24px"} />
+                            </Stack>
+                            <Text fontSize="md" fontWeight="bold">{dApiName}</Text>
+                            <Spacer />
+                        </Stack>
+                        <Text width={"100%"} noOfLines={1} fontSize="xs">{props.dApi.metadata.category}</Text>
+                    </Stack>
+                    <Spacer />
+                    {
+                        isHeader && (isOpen ? <TriangleUpIcon w={6} h={6} /> : <TriangleDownIcon w={6} h={6} />)
+                    }
+
+                </Flex>
+            </Box>
+        </VStack>
+    );
+};
+
+export default Hero;
+
+
