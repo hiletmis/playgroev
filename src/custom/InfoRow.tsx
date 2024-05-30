6c43a3d346a350c95158db491eba27fbdcfb6fba
hiletmis <hiletmis@gmail.com>
2024-05-30 14:39:32 +0300

    Refactor InfoRow component to include link functionality

--- src/custom/InfoRow.tsx
+++ src/custom/InfoRow.tsx
@@ -1,18 +1,26 @@
 import { Text, Flex, Spacer, VStack } from '@chakra-ui/react';
 import { COLORS } from '../helpers/utils';
+import { ViewIcon } from '@chakra-ui/icons';
 
-const Hero = (props: any) => {
-    const { header, text, bgColor = COLORS.app } = props;
+const InfoRow = (props: any) => {
+    const { header, text, link, bgColor = COLORS.app } = props;
     return (
         <VStack spacing={1} p={4} width={"100%"} bgColor={bgColor} marginBottom={"5px"} borderRadius={"10"} alignItems={"left"}>
             <Text fontWeight={"bold"} fontSize={"sm"}>{header}</Text>
 
             <Flex className='box'>
-                <Text fontSize={"md"}>{text}</Text>
+                <Text fontSize={"sm"}>{text}</Text>
                 <Spacer />
+                {
+                    link &&
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                        <ViewIcon w={6} h={6} />
+                    </a>
+                }
+
             </Flex>
         </VStack>
     );
 };
 
-export default Hero;
+export default InfoRow;
