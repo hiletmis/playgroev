1cdacf9f9cf102df30f0c3447ad848711ee659e4
hiletmis <hiletmis@gmail.com>
2024-05-30 14:38:51 +0300

    Add ErrorRow component for displaying error information

---
+++ src/custom/ErrorRow.tsx
@@ -0,0 +1,17 @@
+import { Text, Box, VStack } from '@chakra-ui/react';
+import { COLORS } from '../helpers/utils';
+
+const ErrorRow = ({ header, text, margin = 0, bgColor = COLORS.caution }: any) => {
+    return (
+        text == null ? null :
+            <VStack direction="row" bgColor={COLORS.main} align="left" borderRadius={"10"} width={"100%"} height={"auto"} m={margin}>
+                <Text fontWeight={"bold"} fontSize={"sm"}>{header}</Text>
+                <Box p="1" width={"100%"} bgColor={bgColor} marginBottom={"5px"} borderRadius={"10"} alignItems={"center"}>
+                    <Text marginLeft={"2"} fontSize={"xs"}>{text}</Text>
+
+                </Box>
+            </VStack>
+    );
+};
+
+export default ErrorRow;
