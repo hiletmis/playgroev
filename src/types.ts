fcf8b4be65f386278d2213e42bd0c0e8f6f76882
hiletmis <hiletmis@gmail.com>
2024-05-30 14:40:29 +0300

    Update types.ts with BidCondition and EncodeBidDetailsArgs

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,5 @@
 import * as z from "zod";
 
-
 export const DataFeedSchema = z.object({
     "p1": z.string(),
     "p2": z.string(),
@@ -10,3 +9,19 @@ export const DataFeedSchema = z.object({
     "beneficiaryAddress": z.string(),
 });
 export type DataFeed = z.infer<typeof DataFeedSchema>;
+
+export type BidCondition = {
+    readonly onchainIndex: bigint;
+    readonly description: BidConditionDescription;
+};
+
+
+// Auction-creator/Auction-cop state
+export type BidConditionDescription = 'GTE' | 'LTE';
+
+export type EncodeBidDetailsArgs = {
+    bidType: string;
+    proxyAddress: `0x${string}`;
+    conditionValue: bigint;
+    updaterAddress: `0x${string}`;
+};
