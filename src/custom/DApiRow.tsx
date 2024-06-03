98405a88dbcfb24fd596a7dbd64056f2d1a8f292
hiletmis <hiletmis@gmail.com>
2024-06-03 16:55:42 +0300

    Add loading spinner to DApiRow component

--- src/custom/DApiRow.tsx
+++ src/custom/DApiRow.tsx
@@ -2,17 +2,19 @@ import { Text, Stack, Box, Image, Flex, Spacer, VStack } from '@chakra-ui/react'
 import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
 import { COLORS } from '../helpers/utils';
 import { SymbolLogo } from '@api3/logos';
+import { ColorRing } from 'react-loader-spinner';
 
 const Hero = (props: any) => {
     const dApiName = props.dApi.name;
     const isHeader = props.isHeader || false;
     const isOpen = props.isOpen || false;
+    const isLoading = props.isLoading || false;
     const symbols = dApiName.split('/');
 
     return (
         <VStack width={"100%"} alignItems={"left"} cursor={"pointer"} onClick={isHeader ? props.onClick : () => { props.setDapi(props.dApi) }}>
             <Box p="3" width={"100%"} height={"70px"} bgColor={props.bgColor ? props.bgColor : isOpen ? COLORS.selectDarker : isHeader ? COLORS.app : COLORS.main} alignItems={"left"}>
-                <Flex className='box' alignItems={"center"}>
+                <Flex className='box' alignItems={"center"} gap={5}>
                     <Stack direction="column" spacing={"2"} width={"100%"}>
                         <Stack direction="row" spacing={"2"} >
                             <Stack direction="row" spacing={"1"}>
@@ -26,7 +28,8 @@ const Hero = (props: any) => {
                     </Stack>
                     <Spacer />
                     {
-                        isHeader && (isOpen ? <TriangleUpIcon w={6} h={6} /> : <TriangleDownIcon w={6} h={6} />)
+                        isLoading ? <ColorRing height="50px" width="50px" ariaLabel="loading" visible={isLoading} />
+                            : isHeader && (isOpen ? <TriangleUpIcon w={6} h={6} /> : <TriangleDownIcon w={6} h={6} />)
                     }
 
                 </Flex>
