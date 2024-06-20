c704fe2fb65c405ae2c22439c8a127f6e7b54529
hiletmis <hiletmis@gmail.com>
2024-06-20 15:48:46 +0300

    Refactor OEVContext to add new states and update bid-related states in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,20 @@ import { VStack, Flex } from '@chakra-ui/react';
 import Header from './components/Header';
 import Welcome from './components/Welcome';
 import { OevContext } from './OEVContext';
+import { BidPrices, BidInfo } from './types';
 
 function App() {
 
   const [address, setAddress] = useState("");
   const [balance, setBalance] = useState(BigInt(0));
+  const [stage, setStage] = useState(0);
+  const [prices, setPrices] = useState({} as BidPrices);
+  const [isBiddable, setIsBiddable] = useState(false);
+  const [bid, setBid] = useState(undefined as BidInfo | undefined);
 
   return (
     <HashRouter>
-      <OevContext.Provider value={{ address, setAddress, balance, setBalance }}>
+      <OevContext.Provider value={{ address, setAddress, balance, setBalance, stage, setStage, prices, setPrices, isBiddable, setIsBiddable, bid, setBid }}>
         <Header />
         <Flex h="calc(100vh - 90px)" p={5} alignItems={'stretch'} flexDirection={'row'}>
           <VStack width={'100%'} alignItems={'center'}>
