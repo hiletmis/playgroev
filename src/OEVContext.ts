b417bc5c49a4c68b8e15389d2e88240b78a1d920
hiletmis <hiletmis@gmail.com>
2024-06-20 15:48:36 +0300

    Refactor OEVContext to add new states

--- src/OEVContext.ts
+++ src/OEVContext.ts
@@ -1,10 +1,19 @@
 import { createContext } from 'react';
+import { BidInfo, BidPrices } from './types';
 
 export interface AppContext {
     address: string;
     setAddress: (wallet: string) => void;
     balance: bigint;
     setBalance: (balance: bigint) => void;
+    stage: number;
+    setStage: (stage: number) => void;
+    prices: BidPrices;
+    setPrices: (prices: BidPrices) => void;
+    isBiddable: boolean;
+    setIsBiddable: (isBiddable: boolean) => void;
+    bid?: BidInfo;
+    setBid: (bid?: BidInfo) => void;
 }
 
 export const OevContext = createContext<AppContext>({} as AppContext);
