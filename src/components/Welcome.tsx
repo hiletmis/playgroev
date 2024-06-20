b135f86dfe8160638b0d4673b5b4a768d425dd7a
hiletmis <hiletmis@gmail.com>
2024-06-20 15:51:48 +0300

    Add Deposit, PlaceBid, AwardUpdate, and Report components

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,9 +1,22 @@
 import { VStack, Tab, Tabs, TabList, TabPanel, TabPanels } from '@chakra-ui/react';
 
 import Bridge from './Bridge';
+import Deposit from './Deposit';
 import PlaceBid from './PlaceBid';
+import { useContext, useEffect } from 'react';
+import { OevContext } from '../OEVContext';
+import AwardUpdate from './AwardUpdate';
+import Report from './Report';
+import { StageEnum } from '../types';
 
 const Welcome = () => {
+
+    const { stage } = useContext(OevContext);
+
+    useEffect(() => {
+        console.log(stage);
+    }, [stage]);
+
     return (
         <VStack
             p={10}
@@ -17,8 +30,11 @@ const Welcome = () => {
         >
             <Tabs>
                 <TabList>
-                    <Tab>Deposit</Tab>
-                    <Tab>Bid</Tab>
+                    <Tab isDisabled={stage < StageEnum.SignIn}>Bridge</Tab>
+                    <Tab isDisabled={stage <= StageEnum.Bridge}>Deposit</Tab>
+                    <Tab isDisabled={stage <= StageEnum.Deposit}>Place Bid</Tab>
+                    <Tab isDisabled={stage <= StageEnum.PlaceBid}>Award and Update</Tab>
+                    <Tab isDisabled={stage !== StageEnum.Report}>Report</Tab>
                 </TabList>
                 <TabPanels>
 
@@ -26,7 +42,16 @@ const Welcome = () => {
                         <Bridge />
                     </TabPanel>
                     <TabPanel>
+                        <Deposit />
+                    </TabPanel>
+                    <TabPanel>
                         <PlaceBid />
+                    </TabPanel>
+                    <TabPanel>
+                        <AwardUpdate />
+                    </TabPanel>
+                    <TabPanel>
+                        <Report />
                     </TabPanel>
                 </TabPanels>
             </Tabs>
