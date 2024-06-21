6fc33811e4e1d707d187a8dadce00a161b1fa24f
hiletmis <hiletmis@gmail.com>
2024-06-21 10:52:15 +0300

    Refactor Welcome component and add tab navigation functionality

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,9 +1,9 @@
-import { VStack, Tab, Tabs, TabList, TabPanel, TabPanels } from '@chakra-ui/react';
+import { VStack, Flex, Text, Box } from '@chakra-ui/react';
 
 import Bridge from './Bridge';
 import Deposit from './Deposit';
 import PlaceBid from './PlaceBid';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { OevContext } from '../OEVContext';
 import AwardUpdate from './AwardUpdate';
 import Report from './Report';
@@ -11,11 +11,50 @@ import { StageEnum } from '../types';
 
 const Welcome = () => {
 
-    const { stage } = useContext(OevContext);
+    const { stage, setStage, tab, setTab } = useContext(OevContext);
 
-    useEffect(() => {
-        console.log(stage);
-    }, [stage]);
+    const TabButton = ({ state, caption, isDisabled = false }: any) => {
+        const isSelected = tab === state;
+        const getColor = () => {
+            if (isSelected) {
+                return 'blue.300';
+            }
+            if (isDisabled) {
+                return 'gray.100';
+            }
+            return 'blue.100';
+        }
+
+        const getTextColor = () => {
+            if (isSelected) {
+                return 'white';
+            }
+            if (isDisabled) {
+                return 'gray.300';
+            }
+            return 'black';
+        }
+
+        function checkSetTab(state: number) {
+            if (isDisabled) return
+            console.log(stage, state)
+            if (stage > StageEnum.PlaceBid && state !== stage) {
+                if (window.confirm("Do you really want to leave, you may lose your progress?")) {
+                    setStage(state)
+                    setTab(state)
+                }
+            } else {
+                setTab(state)
+            }
+        }
+
+        return (
+            <VStack onClick={() => checkSetTab(state)} cursor={'pointer'} p={2} bgColor={getColor()}>
+                <Text color={getTextColor()} fontWeight={isSelected ? "bold" : "regular"} fontSize={'md'}>{caption}</Text>
+            </VStack>
+        );
+    };
+
 
     return (
         <VStack
@@ -28,33 +67,33 @@ const Welcome = () => {
             alignItems={'left'}
             justifyItems={'center'}
         >
-            <Tabs>
-                <TabList>
-                    <Tab isDisabled={stage < StageEnum.SignIn}>Bridge</Tab>
-                    <Tab isDisabled={stage <= StageEnum.Bridge}>Deposit</Tab>
-                    <Tab isDisabled={stage <= StageEnum.Deposit}>Place Bid</Tab>
-                    <Tab isDisabled={stage <= StageEnum.PlaceBid}>Award and Update</Tab>
-                    <Tab isDisabled={stage !== StageEnum.Report}>Report</Tab>
-                </TabList>
-                <TabPanels>
+            <VStack alignItems={'left'} width={"100%"} spacing={0}>
+                <Flex gap={2}>
+                    <TabButton isDisabled={stage < StageEnum.SignIn} caption={'Bridge'} state={0} />
+                    <TabButton isDisabled={stage <= StageEnum.Bridge} caption={'Deposit'} state={1} />
+                    <TabButton isDisabled={stage <= StageEnum.Deposit} caption={'Place Bid'} state={2} />
+                    <TabButton isDisabled={stage <= StageEnum.PlaceBid} caption={'Award and Update'} state={3} />
+                    <TabButton isDisabled={stage !== StageEnum.Report} caption={'Report'} state={4} />
+                </Flex>
+                <Box bgColor={"blue.300"} height={"5px"} />
 
-                    <TabPanel>
-                        <Bridge />
-                    </TabPanel>
-                    <TabPanel>
-                        <Deposit />
-                    </TabPanel>
-                    <TabPanel>
-                        <PlaceBid />
-                    </TabPanel>
-                    <TabPanel>
-                        <AwardUpdate />
-                    </TabPanel>
-                    <TabPanel>
-                        <Report />
-                    </TabPanel>
-                </TabPanels>
-            </Tabs>
+            </VStack>
+
+            {
+                tab === 0 && <Bridge />
+            }
+            {
+                tab === 1 && <Deposit />
+            }
+            {
+                tab === 2 && <PlaceBid />
+            }
+            {
+                tab === 3 && <AwardUpdate />
+            }
+            {
+                tab === 4 && <Report />
+            }
         </VStack>
     );
 };
