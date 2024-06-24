afa19032ab1de86e7f14f19d3362ce06bb1a29f1
hiletmis <hiletmis@gmail.com>
2024-06-24 10:44:26 +0300

    Remove confusing elements

--- src/custom/AddCollateral.tsx
+++ src/custom/AddCollateral.tsx
@@ -1,38 +1,23 @@
-import { Button, Box, Flex, VStack, NumberInput, NumberInputField, Spacer, Image, Text } from '@chakra-ui/react';
+import { Box, Flex, VStack, NumberInput, NumberInputField, Spacer, Image } from '@chakra-ui/react';
 import { COLORS, sanitizeAmount } from "../helpers/utils";
-import { SymbolLogo } from "@api3/logos";
+import { ChainLogo } from "@api3/logos";
 
 const AddCollateral = (props: any) => {
     const { setTokenAmount, tokenAmount, tokenBalance, bgColor = COLORS.app } = props;
 
+    const isBalance = () => {
+        return parseFloat(tokenBalance) > parseFloat(tokenAmount)
+    }
+
     return (
         <Box width={"100%"} bgColor={bgColor} >
             <VStack spacing={3} direction="row" align="left" m="1.2rem">
-                <Flex>
-                    <NumberInput value={tokenAmount} step={1} min={0} size={"lg"} onChange={(valueString) => { sanitizeAmount(valueString, setTokenAmount) }}><NumberInputField borderWidth={"0px"} placeholder="0.0" fontSize={"4xl"} inputMode="numeric" /></NumberInput>
+                <Flex alignItems={"center"}>
+                    <NumberInput value={tokenAmount} color={isBalance() ? "black" : "red"} step={1} min={0} size={"lg"} onChange={(valueString) => { sanitizeAmount(valueString, setTokenAmount) }}><NumberInputField borderWidth={"0px"} placeholder="0.0" fontSize={"4xl"} inputMode="numeric" /></NumberInput>
                     <Spacer />
-                    <Image src={SymbolLogo("ETH", true)} width={"40px"} height={"40px"} />
+                    <Image src={ChainLogo("4913", true)} width={"40px"} height={"40px"} />
                 </Flex>
 
-                <Flex gap={3} alignItems={"center"}>
-                    <Text
-                        color={parseFloat(tokenBalance) < parseFloat(tokenAmount) ? "red.500" : "black"}
-                        fontWeight={"bold"}
-                        visibility={tokenBalance === "0" ? "hidden" : "visible"}
-                        fontSize={"md"}>
-                        {parseFloat(tokenBalance) < parseFloat(tokenAmount) ? "Insufficient Balance" : "Ethereum Balance"}
-                    </Text>
-                    <Spacer />
-                    <Button
-                        size={"xs"} colorScheme={"green"} variant={"outline"}
-                        onClick={() => window.open('https://oev-network.bridge.caldera.xyz/', '_blank')}>Add Funds
-                    </Button>
-                    <Text
-                        cursor={'pointer'}
-                        onClick={() => {
-                            setTokenAmount(tokenBalance)
-                        }} fontWeight={"bold"} fontSize={"lg"}>{tokenBalance} ETH</Text>
-                </Flex>
 
             </VStack>
         </Box>
