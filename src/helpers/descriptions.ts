afde67730b5d652a995660fd630e14f8f498d9be
hiletmis <hiletmis@gmail.com>
2024-06-24 12:02:56 +0300

    Add descriptions utility

---
+++ src/helpers/descriptions.ts
@@ -0,0 +1,11 @@
+export const bridgeTitle = "Bridge"
+export const bridgeDescriptionLine1 = "Bridge your Ethereum to OEV Network."
+export const bridgeDescriptionLine2 = "You can add funds to your wallet by using the official OEV Network bridge."
+
+export const oevNetworkBalance = "OEV Network Balance"
+export const ethereumBalance = "Ethereum Balance"
+
+export const noBalanceTitle = "Add Funds"
+export const noBalanceDescription = "You have no funds to deposit. Please add funds to your wallet."
+
+export const bridgeButton = "OEV Network Bridge"
