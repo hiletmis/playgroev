22612e94d7c612982b24d37068fd26a7aec95661
hiletmis <hiletmis@gmail.com>
2024-06-24 16:15:30 +0300

    Refactor Heading component to include help icon

--- src/custom/Heading.tsx
+++ src/custom/Heading.tsx
@@ -1,14 +1,15 @@
-import { Text, Heading, Flex, Spacer, VStack } from '@chakra-ui/react';
+import { Text, Heading, Flex, Spacer, VStack, Image } from '@chakra-ui/react';
 import { ColorRing } from 'react-loader-spinner';
 
 const CustomHeading = (props: any) => {
     const { header, description, isLoading } = props;
     return (
-        <VStack alignItems={"left"} spacing={3} >
-            <Flex>
+        <VStack maxW={"700px"} borderWidth="px" flex="1" alignItems={"left"}>
+            <Flex alignItems={"center"}>
                 <Heading size={"lg"}>{header}</Heading>
                 <Spacer />
                 <ColorRing height="30px" width="30px" ariaLabel="loading" visible={isLoading} />
+                <Image src={'./help.svg'} width={"30px"} height={"30px"} cursor={"pointer"} />
             </Flex>
             <Text fontSize={"sm"}>{description}</Text>
         </VStack>
