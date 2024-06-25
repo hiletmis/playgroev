2d91b07d6762998fb4b224ab475339e54207b388
hiletmis <hiletmis@gmail.com>
2024-06-25 17:02:38 +0300

    refactor: Remove unnecessary code in SignIn component

--- src/custom/SignIn.tsx
+++ src/custom/SignIn.tsx
@@ -1,19 +1,11 @@
 import { VStack, Flex, Spacer, Image } from '@chakra-ui/react';
 import CustomHeading from './Heading';
-import { useContext } from 'react';
-import { OevContext } from '../OEVContext';
-import { StageEnum } from '../types';
 
 const SignIn = () => {
-    const { setStage } = useContext(OevContext);
-
-    setStage(StageEnum.SignIn);
-
     return (
         <VStack spacing={4} alignItems={"left"} >
             <Flex>
                 <CustomHeading header={"Connect Wallet"} description={"Connect your wallet to continue."} isLoading={false}></CustomHeading>
-
                 <Spacer />
                 <Image src={`./caution.svg`} width={"30px"} height={"30px"} />
             </Flex>
