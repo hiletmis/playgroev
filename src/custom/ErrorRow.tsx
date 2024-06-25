5e4f079a7f91201faa54ef6c9fc2040280d63ce3
hiletmis <hiletmis@gmail.com>
2024-06-25 13:20:04 +0300

    Refactor ErrorRow component to simplify conditional rendering and improve code readability

--- src/custom/ErrorRow.tsx
+++ src/custom/ErrorRow.tsx
@@ -3,13 +3,13 @@ import { COLORS } from '../helpers/utils';
 
 const ErrorRow = ({ header, text, margin = 0, bgColor = COLORS.caution }: any) => {
     return (
-        text == null ? null :
-            <VStack direction="row" align="left" width={"100%"} height={"auto"} m={margin}>
-                <Text fontWeight={"bold"} fontSize={"md"}>{header}</Text>
-                <Box p="2" width={"100%"} bgColor={bgColor} marginBottom={"5px"} alignItems={"center"}>
-                    <Text marginLeft={"2"} fontSize={"md"}>{text}</Text>
-                </Box>
-            </VStack>
+        text &&
+        <VStack direction="row" align="left" width={"100%"} height={"auto"} m={margin}>
+            <Text fontWeight={"bold"} fontSize={"md"}>{header}</Text>
+            <Box p="2" width={"100%"} bgColor={bgColor} marginBottom={"5px"} alignItems={"center"}>
+                <Text marginLeft={"2"} fontSize={"md"}>{text}</Text>
+            </Box>
+        </VStack>
     );
 };
 
